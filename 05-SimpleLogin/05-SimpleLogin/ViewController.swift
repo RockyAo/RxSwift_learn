92bc685b077910fd33d9557aacc9c69f44f6ea25
Rocky <aoyun@zhongchebaolian.com>
2017-03-17 18:40:42 +0800

    登录

--- 05-SimpleLogin/05-SimpleLogin/ViewController.swift
+++ 05-SimpleLogin/05-SimpleLogin/ViewController.swift
@@ -7,12 +7,73 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+
+let minAccountLength = 5
+let minPasswordLength = 5
+
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var accountTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var accountNameLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
+     fileprivate let disposebag = DisposeBag()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        accountNameLabel.text = "用户名至少 \( minAccountLength) 位"
+        passwordLabel.text = "密码至少 \( minPasswordLength) 位"
+        
+        let accountValid = accountTextfield.rx.text.orEmpty
+            .map{ $0.characters.count >= minAccountLength}
+            .shareReplay(1)
+        
+        let passwordValid = passwordTextfield.rx.text.orEmpty
+            .map{ $0.characters.count >= minPasswordLength}
+            .shareReplay(1)
+        
+        
+        let finalValid = Observable.combineLatest(accountValid,passwordValid) {   $0 && $1  }
+            .shareReplay(1)
+        
+        
+        accountValid
+            .bindTo(passwordTextfield.rx.isEnabled)
+            .disposed(by: disposebag)
+        
+        accountValid
+            .bindTo(accountNameLabel.rx.isHidden)
+            .disposed(by: disposebag)
+        
+        passwordValid
+            .bindTo(passwordLabel.rx.isHidden)
+            .disposed(by: disposebag)
+        
+        finalValid
+            .bindTo(loginButton.rx.isEnabled)
+            .disposed(by: disposebag)
+        
+        loginButton.rx.tap
+            .subscribe(onNext: { [weak self] in self?.showAlert() })
+            .disposed(by: disposebag)
+        
+        
+        
+    }
+    
+    
+    func showAlert() {
+    
+        let alert = UIAlertView(title:"登录成功", message: nil, delegate: nil, cancelButtonTitle: "确定")
+        
+        alert.show()
+        
     }
 
     override func didReceiveMemoryWarning() {
