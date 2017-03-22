57ba26218a18ba8261d79eb89ed79071e1cf7bbd
Rocky <aoyun@zhongchebaolian.com>
2017-03-22 18:03:02 +0800

    初始化计算器项目

---
+++ 06-Calculator/06-Calculator/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  06-Calculator
+//
+//  Created by Rocky on 2017/3/22.
+//  Copyright © 2017年 Rocky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var lastSignLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var allClearButton: UIButton!
+    @IBOutlet weak var changeSignButton: UIButton!
+    @IBOutlet weak var percentButton: UIButton!
+    
+    @IBOutlet weak var divideButton: UIButton!
+    @IBOutlet weak var multiplyButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var equalButton: UIButton!
+    
+    @IBOutlet weak var dotButton: UIButton!
+    @IBOutlet weak var zeroButton: UIButton!
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var nineButton: UIButton!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+
+}
+
